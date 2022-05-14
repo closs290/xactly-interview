4c85845b499d7032699b026d7d1acab1227f6ec8
Reuben M. Closson <closs290@regis.edu>
2022-05-13 20:52:11 -0700

    Removed React app as submodule; added as repo code

---
+++ xactly-app/src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import { Tree, TreeNode } from 'react-organizational-chart';
+
+const ExampleTree = () => (
+  <Tree label={<div>Root</div>}>
+    <TreeNode label={<div>Child 1</div>}>
+      <TreeNode label={<div>Grand Child</div>} />
+    </TreeNode>
+  </Tree>
+);
+
+const StyledNode = styled.div`
+  padding: 5px;
+  border-radius: 8px;
+  display: inline-block;
+  border: 1px solid red;
+`;
+
+const StyledTreeExample = () => (
+  <Tree
+    lineWidth={'2px'}
+    lineColor={'green'}
+    lineBorderRadius={'10px'}
+    label={<StyledNode>Root</StyledNode>}
+  >
+    <TreeNode label={<StyledNode>Child 1</StyledNode>}>
+      <TreeNode label={<StyledNode>Grand Child</StyledNode>} />
+    </TreeNode>
+    <TreeNode label={<StyledNode>Child 2</StyledNode>}>
+      <TreeNode label={<StyledNode>Grand Child</StyledNode>}>
+        <TreeNode label={<StyledNode>Great Grand Child 1</StyledNode>} />
+        <TreeNode label={<StyledNode>Great Grand Child 2</StyledNode>} />
+      </TreeNode>
+    </TreeNode>
+    <TreeNode label={<StyledNode>Child 3</StyledNode>}>
+      <TreeNode label={<StyledNode>Grand Child 1</StyledNode>} />
+      <TreeNode label={<StyledNode>Grand Child 2</StyledNode>} />
+    </TreeNode>
+  </Tree>
+);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ExampleTree />
+        <StyledTreeExample />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
